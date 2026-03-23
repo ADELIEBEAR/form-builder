93013a9db58ba6030de51839e56ba27663415acb
ADELIEBEAR <qwfgnmk78@gmail.com>
2026-03-23 12:55:51 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AuthProvider } from './hooks/useAuth'
 import { ThemeProvider } from './lib/themeContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </BrowserRouter>
 )
