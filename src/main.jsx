56dbd0fb8004eae55791a7cf53b76f3aafff6e57
ADELIEBEAR <qwfgnmk78@gmail.com>
2026-05-31 08:38:20 +0900

    Load duplicate and AI UI fixes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App'
 import { AuthProvider } from './hooks/useAuth'
 import { ThemeProvider } from './lib/themeContext'
 import './index.css'
+import './fixes-duplicates-ai.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
